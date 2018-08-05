c398daafee58635fa12b323eacad846cffb34e72
haseungwon <hapeter@naver.com>
2018-08-05 20:56:35 +0900

    캐릭터 생성과 걷기--마우스 코딩

---
+++ 유니티2P게임제작/Assets/scripts/stickman_move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stickman_move : MonoBehaviour {
+    Animator anim;
+
+	
+	void Start () {
+        anim = transform.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float f = Input.GetAxis("Horizontal");
+        if (f < 0)
+        {
+            transform.parent.localScale=new Vector3 (-1, 1, 1);
+        }
+        else if (f>0)
+        {
+            transform.parent.localScale = new Vector3(1, 1, 1);
+        }
+        f = Mathf.Abs(f);
+        anim.SetFloat("speed", f);
+	}
+}
