bf9bf185caae9d25f2e45c0998e4cb931b4f8e80
kej <kej11070222@gmail.com>
2018-09-17 19:55:31 +0900

    유니티를 이용한 Catch 게임 개발 테스트(공부중) : 현재 캐릭터 좌우 이동과 마우스 캐릭터가 마우스 포인터 따라오기 까지 진행

---
+++ Study/kej1234/Catch_Test/Assets/Scripts/MouseInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInfo : MonoBehaviour {
+	void FixedUpdate () {
+		Vector3 pz = Camera.main.ScreenToWorldPoint (Input.mousePosition); 
+		pz.z = 0; 
+		gameObject.transform.position = pz;
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.CompareTag("Player")){
+			Debug.Log("Game Over");
+			GameObject.Find("GameManager").GetComponent<Menu>().GameOver();
+		}
+	}
+}
---
+++ Study/kej1234/Catch_Test/Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour {
+	public float movePower=1f;
+	public float jumpPower=1f;
+
+	Rigidbody2D rigid;
+	Animator animator;
+
+	void Start(){
+		rigid=gameObject.GetComponent<Rigidbody2D>();
+		animator=gameObject.GetComponentInChildren<Animator>();
+	}
+
+	void Update()
+	{
+		//moving
+		if (Input.GetAxis("Horizontal")==0){
+			animator.SetBool("isMoving",false);
+		}
+		else{
+			animator.SetBool("isMoving",true);
+		}
+	}
+	void FixedUpdate()
+	{
+		Move();
+	}
+
+	void Move(){
+		Vector3 moveVelocity=Vector3.zero;
+
+		if (Input.GetAxisRaw("Horizontal")<0){
+			moveVelocity=Vector3.left;
+
+			transform.localScale=new Vector3(-1,1,1);
+		}
+		else if (Input.GetAxisRaw("Horizontal")>0){
+			moveVelocity=Vector3.right;
+
+			transform.localScale=new Vector3(1,1,1);
+		}
+
+		transform.position+=moveVelocity*movePower*Time.deltaTime;
+	}
+}
