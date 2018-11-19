7436af8adb6182c238753d75576ea7c38921a465
haseungwon <hapeter@naver.com>
2018-11-19 21:54:09 +0900

    시작화면 및 씬 전환

---
+++ Study/kej1234/Catch_Test/Assets/Scripts/buttonstart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class buttonstart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+
+    public void startgame()
+    {
+        SceneManager.LoadScene("Test");
+    }
+
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
