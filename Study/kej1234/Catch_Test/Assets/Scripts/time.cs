98f557f8842f459101c0f10042d1c18b8bb79015
haseungwon <hapeter@naver.com>
2018-11-28 20:46:45 +0900

    타이머 기능 -1

---
+++ Study/kej1234/Catch_Test/Assets/Scripts/time.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class time : MonoBehaviour {
+
+    public Text timelabel;
+    public float timecount = 0;
+    private void Update()
+    {
+        timecount += Time.deltaTime;
+        timelabel.text = string.Format("0:N2", timecount);
+    }
+    
+
+
+}
